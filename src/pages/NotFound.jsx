f6e2393b5f38767a43f2c7f8437b746907c56640
kitsuiwebster <raphmartin1999@gmail.com>
2024-03-14 02:40:18 +0100

    feat: 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,13 @@
 import '../assets/scss/pages/NotFound.scss';
+import pikachu404 from '../assets/images/404.png';
 
 function NotFound() {
     return(
         <div id="error">
             <div className='error'>
-                <h1>404</h1>
-                <h2>Page Not Found</h2>
-                <p>Sorry, the page you are looking for does not exist.</p>
+                <img src={pikachu404} alt="Pikachu" className='error-img'/>
+                <h2 className='error-page'>Page Not Found</h2>
+                <p className='error-sorry'>Sorry, the page you are looking for does not exist 😵‍💫</p>
             </div>
         </div>
     )
