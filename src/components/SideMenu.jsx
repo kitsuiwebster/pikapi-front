7f2675bec76f2d2652b23cbc28856be77b572b0e
kitsuiwebster <raphmartin1999@gmail.com>
2024-03-16 21:02:16 +0100

    wip: side menu

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -10,22 +10,47 @@ import lightAbout from '../assets/images/light-about.png';
 import darkAbout from '../assets/images/dark-about.png';
 import lightContact from '../assets/images/light-contact.png';
 import darkContact from '../assets/images/dark-contact.png';
+import lightPikadex from '../assets/images/light-pikadex.png';
+import darkPikadex from '../assets/images/dark-pikadex.png';
+import lightUser from '../assets/images/light-user.png'; 
+import darkUser from '../assets/images/dark-user.png';
 
 function SideMenu() {
-  const { theme } = useTheme();
-  return (
-    <nav className="side-menu">
-        <Link to="/pikadex">
-            <img loading="lazy" className='side-menu-heart' src={theme === 'dark' ? lightHeart : darkHeart} alt='Favourite Pictures' />
-        </Link>
-        <Link to="/about">
-            <img loading="lazy" className='side-menu-about' src={theme === 'dark' ? lightAbout : darkAbout} alt='About Page' />
-        </Link>
-        <Link to="/contact">
-            <img loading="lazy" className='side-menu-contact' src={theme === 'dark' ? lightContact : darkContact} alt='About Page' />
-        </Link>
-    </nav>
-  );
-}
-
+    const { theme } = useTheme();
+    return (
+      <nav className="side-menu">
+          <Link className='side-menu-link' to="/favourites">
+              <div className="side-menu-item">
+                  <img loading="lazy" className='side-menu-icon' src={theme === 'dark' ? lightHeart : darkHeart} alt='Favourite Pictures' />
+                  <span className="side-menu-text">Favourites</span>
+              </div>
+          </Link>
+          <Link className='side-menu-link' to="/pikadex">
+              <div className="side-menu-item">
+                  <img loading="lazy" className='side-menu-icon' src={theme === 'dark' ? lightPikadex : darkPikadex} alt='Pikadex' />
+                  <span className="side-menu-text">Pikadex</span>
+              </div>
+          </Link>
+          <Link className='side-menu-link' to="/about">
+              <div className="side-menu-item">
+                  <img loading="lazy" className='side-menu-icon' src={theme === 'dark' ? lightAbout : darkAbout} alt='About Page' />
+                  <span className="side-menu-text">About</span>
+              </div>
+          </Link>
+          <Link className='side-menu-link' to="/account">
+              <div className="side-menu-item">
+                  <img loading="lazy" className='side-menu-icon' src={theme === 'dark' ? lightUser : darkUser} alt='Account Page' />
+                  <span className="side-menu-text">Account</span>
+              </div>
+          </Link>
+          <Link className='side-menu-link' to="/contact">
+              <div className="side-menu-item">
+                  <img loading="lazy" className='side-menu-icon' src={theme === 'dark' ? lightContact : darkContact} alt='Contact Page' />
+                  <span className="side-menu-text">Contact</span>
+              </div>
+          </Link>
+      </nav>
+    );
+  }
+  
 export default SideMenu;
