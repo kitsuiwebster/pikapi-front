0c1264ed03b2050ab4ec1f76fe8855426845bea2
kitsuiwebster <raphmartin1999@gmail.com>
2024-03-16 21:00:31 +0100

    feat: favourites page

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../assets/scss/pages/Favourites.scss';
+import '../assets/scss/index.scss';
+
+function Favourites() {
+    return (
+        <>
+            <div id="favourites">
+                <div className='favourites'>
+                    <p>Favourites</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Favourites;
