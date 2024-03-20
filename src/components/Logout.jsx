d854f7c54d07f3c1f3bf022319400b912b1abb99
kitsuiwebster <raphmartin1999@gmail.com>
2024-03-20 20:00:05 +0100

    feat: logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { logout as logoutFn } from '../js/auth'; // Adjust the import path as necessary
-import { useAuth } from '../AuthContext'; // Adjust the import path as necessary
+import { logout as logoutFn } from '../js/auth';
+import { useAuth } from '../AuthContext';
 
 const Logout = () => {
   const navigate = useNavigate();
   const { setIsAuthenticated } = useContext(useAuth);
 
   useEffect(() => {
-    logoutFn(); // This clears the JWT token from localStorage
-    setIsAuthenticated(false); // Update authentication state
+    logoutFn(); 
+    setIsAuthenticated(false); 
     navigate('/login');
   }, [navigate, setIsAuthenticated]);
 
-  return null; // Since we're redirecting, no need to return actual JSX
+  return null; 
 };
 
 export default Logout;
