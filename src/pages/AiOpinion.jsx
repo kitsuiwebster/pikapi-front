555cbf8eb91c9bb311df808ad4bdf898f3882763
kitsuiwebster <raphmartin1999@gmail.com>
2024-04-18 18:56:42 +0200

    feat: ai opinion page

---
+++ src/pages/AiOpinion.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/scss/pages/AiOpinion.scss';
+import '../assets/scss/index.scss';
+
+function AiOpinion() {
+    return (
+        <>
+            <div id="aiopinion">
+                <h1 className='title'>Opinion on AI</h1>
+            </div>
+        </>
+    );
+}
+
+export default AiOpinion;
