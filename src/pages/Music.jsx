bdf42f80c523f82cfe49b04e8094321873ef6270
kitsuiwebster <raphmartin1999@gmail.com>
2024-04-18 18:56:59 +0200

    feat: music page

---
+++ src/pages/Music.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/scss/pages/Music.scss';
+import '../assets/scss/index.scss';
+
+function Music() {
+    return (
+        <>
+            <div id="music">
+                <h1 className='title'>Pikachu Music</h1>
+            </div>
+        </>
+    );
+}
+
+export default Music;
