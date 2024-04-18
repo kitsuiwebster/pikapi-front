d1950b129047ce7e898f830bb597192723b2d0ad
kitsuiwebster <raphmartin1999@gmail.com>
2024-04-18 18:55:26 +0200

    fix: backend url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-export const url = 'https://api.pikapi.co:4001';
+export const url = 'http://localhost:4001';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
