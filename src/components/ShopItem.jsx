8efdfdad23ddb5677f35c1931af7d5219868fa98
kitsuiwebster <raphmartin1999@gmail.com>
2024-06-01 02:36:52 +0200

    feat: shop items mock

---
+++ src/components/ShopItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../assets/scss/components/ShopItem.scss';
+
+function ShopItem({ imagePath, title, price, iconPath }) {
+    return (
+        <div className="shopitem">
+            <img src={imagePath} alt={title} className="shopitem-image" />
+            <h3 className="shopitem-title">{title}</h3>
+            <div className="shopitem-icon">
+                <p className="shopitem-price">${price}</p>
+                <img className='shopitem-icon' src={iconPath} alt="Icon" />
+            </div>
+        </div>
+    );
+}
+
+export default ShopItem;
