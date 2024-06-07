cae3b2eb58f88af9c2094a6dac19bfa0c0614921
kitsuiwebster <raphmartin1999@gmail.com>
2024-06-07 23:37:46 +0200

    feat: phone wallpaper values

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -5,7 +5,9 @@ function ShopItem({ imagePath, title, price, iconPath }) {
     return (
         <div className="shopitem">
             <img src={imagePath} alt={title} className="shopitem-image" />
-            <h3 className="shopitem-title">{title}</h3>
+            <div className='shopitem-container'>
+                <h3 className="shopitem-title">{title}</h3>
+            </div>
             <div className="shopitem-icon">
                 <p className="shopitem-price">${price}</p>
                 <img className='shopitem-icon' src={iconPath} alt="Icon" />
